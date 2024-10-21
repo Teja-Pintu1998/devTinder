bc14aebfb6bbf20e9ca359c36e6c6246e8b10460
Teja <saitejagunjala@gmail.com>
2024-10-20 22:52:29 -0500

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express(); //This creates an instance of web server.
 
 
-app.get("/user/:age", (req, res, next) => {
- const art = req.params.age
-  res.send("got user details successfully - " + art);
-});
+app.use("/login",(req,res,next)=>{
+  console.log("logged in successfully");
+  
+  next();
+  res.send("sent user data RHF 1");  
+},(req,res,next)=>{
+  console.log("logged twice in successfully");
+  res.send("sent user data RHF 2");
+  next();
+})
+
+app.get("/login/user",(req,res,next)=>{
+  res.send("sent user data");
+})
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on the port 3000");
--- src/workOnIt.md
+++ src/workOnIt.md
@@ -11,4 +11,6 @@ order of routes matters. play with diff routes using app.use middleware
 
 install postman, create workspace,collection and try making calls using http methods. try with use,get,post,delete and patch and test in all scenarios. 
 
-explore advances routing techniques like using ?,* and regex in url path and also learn diff between req.params and req.query
+explore advances routing techniques like using ?,* and regex in url path and also learn diff between req.params and req.query.
+
+try using multiple RHFs in app.use and also use next() in dufferent places and check the responses in client-side. try putting next() before res.send and observe the output.
